78dc607070da645d388003d149390424619f64af
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-06 17:18:11 +0200

    LabProblem5,6

--- ObjectsandDOM/06. Format the Text/solution.js
+++ ObjectsandDOM/06. Format the Text/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-    
+    const inputElementValue = document.getElementById("input").innerText;
+
+    const sentences = inputElementValue
+        .replaceAll(".", ".,,")
+        .split(",,").filter(e => e != "")
+        .map(e => e.trimStart());
+
+    const formatedSentences = [];
+    let currSentences = [];
+
+    for (let i = 1; i <= sentences.length; i++) {
+        currSentences.push(sentences[i - 1])
+
+        if (i % 3 == 0) {
+            formatedSentences.push(currSentences);
+            currSentences = [];
+        }
+    }
+
+    formatedSentences.push(currSentences);
+
+    console.log(formatedSentences[2]);
 }
