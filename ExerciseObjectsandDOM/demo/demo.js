46c5b50fb4cab1db46f5555daecf0700983d7523
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-08 21:55:54 +0200

    Exercise

---
+++ ExerciseObjectsandDOM/demo/demo.js
@@ -0,0 +1,34 @@
+// function demo() {
+//     let human = {
+//         name: "Ivo",
+//         age: 25,
+//         gender: "male"
+//     };
+
+//     human.hobby = "sport";
+//     human["weight"] = 81;
+
+//     human.age = 26;
+
+//     delete human.gender;
+
+//     console.log(human);
+
+//     for (const key in human) {
+//         console.log(human[key]);
+//     }
+// }
+
+// demo();
+
+function mainElementManipulator() {
+    // let mainElement = document.getElementsByClassName("single");
+
+    // let coloredText = mainElement[0].style.color = "blue";
+    // let newElement = document.createElement("div");
+    // let textOfNewElement = newElement.textContent = "new text";
+    // let dontNeededElement = document.getElementsByClassName("single")[1];
+    
+    // document.body.removeChild(dontNeededElement);
+    // document.body.appendChild(newElement);
+}
