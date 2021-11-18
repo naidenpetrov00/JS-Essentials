ec56388cac0a2db4f955018cbc60c95abfa885f1
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-18 22:24:58 +0200

    ExerciseProblem2,3

--- ExerciseObjectsandDOM/02. Chat-Room/solution.js
+++ ExerciseObjectsandDOM/02. Chat-Room/solution.js
@@ -1,8 +1,17 @@
-function solve(element) {
-}
+function solve() {
+   let targetDiv = document.getElementsByClassName("my-message")[0];
+   let textArea = document.getElementById("chat_input");
+   let chatMessageArea = document.getElementById("chat_messages");
 
-function send(element) {
-   let messageElement = document.createElement("my-message");
-   messageElement.className = "message";
-   console.log(messageElement);
+   let sendButonElement = document.getElementById("send");
+
+   sendButonElement.addEventListener("click", function (params) {
+      let targetDivClone = targetDiv.cloneNode(true);
+      let textAreaContent = textArea.value;
+      targetDivClone.textContent = textAreaContent;
+
+      chatMessageArea.appendChild(targetDivClone);
+
+      textArea.value = "";
+   });
 }
--- ExerciseObjectsandDOM/03. Number-Convertor/solution.js
+++ ExerciseObjectsandDOM/03. Number-Convertor/solution.js
@@ -1,4 +1,13 @@
 function solve() {
+    const parseOptionsElement = document.getElementById("selectMenuTo");
+    const binaryOpitionElement = parseOptionsElement.getElementsByTagName("option")[0];
+    const hexadecimalOpitionElement = document.createElement("option");
+    parseOptionsElement.appendChild(hexadecimalOpitionElement);
 
-    //TODO...
+    binaryOpitionElement.value = "binary"
+    binaryOpitionElement.textContent = "Binary";
+    hexadecimalOpitionElement.value = "hexadecimal";
+    hexadecimalOpitionElement.textContent = "Hexadecimal";
+
+    console.log(binaryOpitionElement);
 }
