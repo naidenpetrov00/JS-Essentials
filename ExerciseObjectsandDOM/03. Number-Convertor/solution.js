bc850665139f68e7d8d924a7ea46be42e0428c19
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-23 23:07:38 +0200

    ExerciseProblem3Working

--- ExerciseObjectsandDOM/03. Number-Convertor/solution.js
+++ ExerciseObjectsandDOM/03. Number-Convertor/solution.js
@@ -1,20 +1,25 @@
 function solve() {
-    const buttonElement = document.getElementsByTagName("button")[0];
-    buttonElement.addEventListener("click", calculate);
-
     const parseOptionsElement = document.getElementById("selectMenuTo");
     const binaryOpitionElement = parseOptionsElement.getElementsByTagName("option")[0];
     const hexadecimalOpitionElement = document.createElement("option");
     parseOptionsElement.appendChild(hexadecimalOpitionElement);
-
     binaryOpitionElement.value = "binary"
     binaryOpitionElement.textContent = "Binary";
     hexadecimalOpitionElement.value = "hexadecimal";
     hexadecimalOpitionElement.textContent = "Hexadecimal";
 
-}
+    const buttonElement = document.getElementsByTagName("button")[0];
+    buttonElement.addEventListener("click", () => {
+        const inputElement = Number(document.getElementById("input").value);
 
-function calculate() {
-    const inputElement = Number(document.getElementById("input").value);
-    console.log(inputElement);
+        if (parseOptionsElement.value === binaryOpitionElement.value) {
+
+        }
+        else if (parseOptionsElement.value === hexadecimalOpitionElement.value) {
+            
+        }
+    });
+
+
+
 }
