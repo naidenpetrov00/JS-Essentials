337dd5eef09e47b6e7b3c2b5cc17d4249688af8a
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-24 23:09:14 +0200

    ExeciseProblem3,4

--- ExerciseObjectsandDOM/03. Number-Convertor/solution.js
+++ ExerciseObjectsandDOM/03. Number-Convertor/solution.js
@@ -2,7 +2,10 @@ function solve() {
     const parseOptionsElement = document.getElementById("selectMenuTo");
     const binaryOpitionElement = parseOptionsElement.getElementsByTagName("option")[0];
     const hexadecimalOpitionElement = document.createElement("option");
+    let resultElement = document.getElementById("result");
+
     parseOptionsElement.appendChild(hexadecimalOpitionElement);
+
     binaryOpitionElement.value = "binary"
     binaryOpitionElement.textContent = "Binary";
     hexadecimalOpitionElement.value = "hexadecimal";
@@ -13,13 +16,12 @@ function solve() {
         const inputElement = Number(document.getElementById("input").value);
 
         if (parseOptionsElement.value === binaryOpitionElement.value) {
-
+            resultElement.value = inputElement.toString(2);
         }
         else if (parseOptionsElement.value === hexadecimalOpitionElement.value) {
-            
+            resultElement.value = inputElement.toString(16);
         }
+
+
     });
-
-
-
 }
--- ExerciseObjectsandDOM/04. JavaScript-Quizz/solution.js
+++ ExerciseObjectsandDOM/04. JavaScript-Quizz/solution.js
@@ -1,3 +1,22 @@
 function solve() {
-  //TODO...
+  function onClick() {
+    if (this.textContent === trueAnswers[sectionCounter]) {
+    }
+
+    sectoinElements[sectionCounter].style.display = "none";
+    sectoinElements[sectionCounter + 1].style.display = "block";
+    sectionCounter++;
+  }
+
+  const sectoinElements = document.getElementsByTagName("section");
+  let sectionCounter = 0;
+
+  const trueAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+
+  const answerElements = document.getElementsByTagName("p");
+
+  for (let answerElement of answerElements) {
+    answerElement.addEventListener("click", onClick)
+  }
 }
+
