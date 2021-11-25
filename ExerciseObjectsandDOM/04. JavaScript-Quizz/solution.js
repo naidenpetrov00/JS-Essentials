9650a95cfa6a9c3f20321982ec8ef8a0a6f5fbfe
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-25 22:14:18 +0200

    ExerciseProblem4Solved

--- ExerciseObjectsandDOM/04. JavaScript-Quizz/solution.js
+++ ExerciseObjectsandDOM/04. JavaScript-Quizz/solution.js
@@ -1,19 +1,38 @@
 function solve() {
   function onClick() {
-    if (this.textContent === trueAnswers[sectionCounter]) {
+    if (this.textContent === correctAnswers[sectionCounter]) {
+      correctAnswersCounter++;
     }
 
-    sectoinElements[sectionCounter].style.display = "none";
-    sectoinElements[sectionCounter + 1].style.display = "block";
-    sectionCounter++;
+    if (sectionCounter === sectoinElements.length - 1) {
+      sectoinElements[sectionCounter].style.display = "none";
+      resultElement.style.display = "block";
+      sectionCounter++;
+
+      if (correctAnswersCounter === correctAnswers.length) {
+        resultValueElement.textContent = "You are recognized as top JavaScript fan!";
+      }
+      else if(correctAnswersCounter < correctAnswers.length){
+        resultValueElement.textContent = `You have ${correctAnswersCounter} right answers`;
+      }
+    }
+    else if (sectionCounter < sectoinElements.length - 1) {
+      sectoinElements[sectionCounter].style.display = "none";
+      sectoinElements[sectionCounter + 1].style.display = "block";
+      sectionCounter++;
+    }
   }
 
-  const sectoinElements = document.getElementsByTagName("section");
-  let sectionCounter = 0;
-
-  const trueAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
-
+  const resultElement = document.getElementById("results");
+  const resultValueElement = resultElement.getElementsByClassName("results-inner")[0];
   const answerElements = document.getElementsByTagName("p");
+  const sectoinElements = document.getElementsByTagName("section");
+
+  let sectionCounter = 0;
+  let correctAnswersCounter = 0;
+
+  const correctAnswers = ["onclick", "JSON.stringify()", "A programming API for HTML and XML documents"];
+
 
   for (let answerElement of answerElements) {
     answerElement.addEventListener("click", onClick)
