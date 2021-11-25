e862c01d797905dec8926cd69b56ca9fba6bc669
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-25 23:41:34 +0200

    ExerciseProblem5Solved

--- ExerciseObjectsandDOM/05. Table-Search-Engine/solution.js
+++ ExerciseObjectsandDOM/05. Table-Search-Engine/solution.js
@@ -1,3 +1,31 @@
 function solve() {
-   //TODO...
+   function onClick() {
+      let counter = selectedElements.length - 1;
+
+      for (let i = counter; i >= 0; i--) {
+         selectedElements[i].removeAttribute("class");
+      }
+
+      const searchValue = inputElement.value;
+
+      const allNames = document.querySelectorAll("tbody > tr > td");
+
+      for (const name in allNames) {
+
+         if (allNames.hasOwnProperty(name)) {
+            const element = allNames[name].textContent;
+
+            if (element.toLowerCase().includes(searchValue.toLowerCase())) {
+               allNames[name].classList.add("select");
+            }
+         }
+      }
+
+      inputElement.value = "";
+   }
+
+   const selectedElements = document.getElementsByClassName("select");
+   const inputElement = document.getElementById("searchField");
+   const buttonElement = document.getElementById("searchBtn");
+   buttonElement.addEventListener("click", onClick);
 }
