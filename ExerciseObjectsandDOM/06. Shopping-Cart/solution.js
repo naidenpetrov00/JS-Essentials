3e0410983655f05b62ec530a3721fa57887e9dad
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-26 18:58:59 +0200

    ExerciseProblem5Solving

--- ExerciseObjectsandDOM/06. Shopping-Cart/solution.js
+++ ExerciseObjectsandDOM/06. Shopping-Cart/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-   //TODO...
+   function onClick() {
+      console.log(this.parentNode);
+   }
+
+   const addButtonElements = document.getElementsByClassName("add-product");
+
+   for (const button of addButtonElements) {
+      button.addEventListener("click", onClick);
+   }
+
 }
