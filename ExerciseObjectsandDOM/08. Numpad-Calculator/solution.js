9e2c65801b8f4bae773f738eca3f8e0f7a8123ba
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-11-30 22:42:30 +0200

    ExerciseProblem8Solving

--- ExerciseObjectsandDOM/08. Numpad-Calculator/solution.js
+++ ExerciseObjectsandDOM/08. Numpad-Calculator/solution.js
@@ -1,4 +1,34 @@
 function solve() {
+    function onClickClear() {
 
-    //TODO...
+    }
+
+    function onClickPad() {
+        expressionOutputElements.textContent = expressionOutputElements.textContent.concat(this.value);
+    }
+
+    function onClickCalculate() {
+        console.log(expressionOutputElements.textContent);
+        for (let element of expressionOutputElements.textContent) {
+            if (!isNaN(element)) {
+                element = Number(element);
+            }
+        }
+    }
+
+    const buttonElements = document.getElementsByTagName("button");
+    const expressionOutputElements = document.getElementById("expressionOutput");
+
+    for (const button of buttonElements) {
+        if (button.value === "Clear") {
+            button.addEventListener("click", onClickClear);
+        }
+        else if (button.value === "=") {
+            button.addEventListener("click", onClickCalculate);
+        }
+        else {
+            button.addEventListener("click", onClickPad);
+        }
+
+    }
 }
