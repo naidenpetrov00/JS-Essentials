6f121decc4b11d90b3add65ae2cac5ab79b473f9
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-12-17 21:02:50 +0200

    Exercise_problem_6

--- ExerciseArraysandMatrices/SortArray.js
+++ ExerciseArraysandMatrices/SortArray.js
@@ -1,7 +1,11 @@
 function solve(input) {
-    
+    input.sort((a, b) => {
+        return a.length - b.length || a.localeCompare(b);
+    }).forEach(i => console.log(i));
 }
 
 solve(
-
+    ['alpha',
+        'beta',
+        'gamma']
 );
