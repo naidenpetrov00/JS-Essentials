6226e4297a3ff46b7e8cbb9cde33b5bbdbd989b1
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2021-12-25 20:21:18 +0200

    Lab

--- StringsAndRegExp/03_Split-String-Equally/solution.js
+++ StringsAndRegExp/03_Split-String-Equally/solution.js
@@ -1,3 +1,17 @@
 function solve() {
-    //TODO...
+    let text = document.getElementById('text').value;
+    const groupSize = Number(document.getElementById('number').value);
+
+    if (text.length % groupSize > 0) {
+        const remainder = text.length % groupSize;
+        const charsToFIll = groupSize - remainder;
+        text = text + text.substr(0, charsToFIll);
+    }
+
+    const result = [];
+    for (let i = 0; i < text.length; i += groupSize) {
+        result.push(text.substr(i, groupSize));
+    }
+
+    console.log(result.join(' '));
 }
--- StringsAndRegExp/04_Replace-a-Certain-Word/solution.js
+++ StringsAndRegExp/04_Replace-a-Certain-Word/solution.js
@@ -1,4 +1,9 @@
 function solve() {
+    const word = document.getElementById('word').value;
+    const text = document.getElementById('text').value;
+    let parts = JSON.parse(text);
 
-    //TODO...
+    const wordToReplace = parts[0].split(' ')[2];
+    const patern = new ReqExp(wordToReplace, 'gi')
+    parts = parts.map(part => part.replace(patern, word))
 }
