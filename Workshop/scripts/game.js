bfe3f1e5c3c68be17a7f73a754fcc1588e6655ec
naidenpetrov0 <naiden.petrov.31.12.00@gmail.com>
2022-01-27 23:30:34 +0200

    Workshop

--- Workshop/scripts/game.js
+++ Workshop/scripts/game.js
@@ -1,23 +1,52 @@
 (function () {
+    const initRenderer = function (ctx) {
+        this.ctx = ctx;
+        return this;
+    }
+
+    const rendererDot = function (left, top) {
+        const { ctx } = this;
+        ctx.fillRect(left, top, 15, 15);
+    };
+
+    const renderer = {
+        init: initRenderer,
+        rendererDot,
+    }
+
     const init = function (selector, width, height) {
         const gameContainer = document.querySelector(selector);
         const canvas = document.createElement('canvas');
         canvas.width = width;
         canvas.height = height;
         gameContainer.appendChild(canvas);
-
-        const ctx = canvas.getContext('2d')
-
+        const ctx = canvas.getContext('2d');
+        this.renderer = renderer.init(ctx);
         return this;
     };
 
-    const start = function () {
+    const dot = {
+        left: 0,
+        top: 100,
+    };
 
+    const gameLoop = function () {
+        const { left, top } = dot;
+        this.renderer.rendererDot(left, top);
+        dot.left += 5;
+        setTimeout(() => {
+            this.gameLoop();
+        }, 100);
+    }
+
+    const start = function () {
+        this.gameLoop();
     };
 
     const game = {
         init,
         start,
+        gameLoop,
     };
 
     window.game = game;
